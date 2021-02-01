ee84138369146bad84d2fe744f2dd011c8353ce7
MizanyHope <64071542+MizanyHope@users.noreply.github.com>
2021-02-01 17:57:12 +0100

    all features + css (nbproducts messed up)

--- client/portfolio.js
+++ client/portfolio.js
@@ -4,12 +4,25 @@
 // current products on the page
 let currentProducts = [];
 let currentPagination = {};
+let currentFilters = {brand:"all", reasonable: false, recent:false, sort:"null"};
+let favoriteProducts = [];
 
 // inititiqte selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
+const selectBrand = document.querySelector('#brand-select');
+const selectReasonable = document.querySelector('#reasonable-check');
+const selectRecent = document.querySelector('#recent-check');
+const selectSort = document.querySelector('#sort-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
+const spanNbDisplayedProducts = document.querySelector('#nbDisplayedProducts');
+const p50 = document.querySelector('#p50');
+const p90 = document.querySelector('#p90');
+const p95 = document.querySelector('#p95');
+var fav = document.querySelectorAll('.fav-check');
+var lastRelease = document.querySelector('#last-release');
+
 
 /**
  * Set global value
@@ -46,33 +59,142 @@ const fetchProducts = async (page = 1, size = 12) => {
   }
 };
 
+
+/*
+ Small functions
+*/
+//Subtract 14 days to th actual date and return it
+Date.prototype.minusDays = function(days) {
+  this.setDate(this.getDate() - parseInt(days));
+  return this;
+};
+//Calcultate x percentile (price) of a list
+const percentile = (arr, q) => {
+  var k = Object.assign([], arr);
+  const sorted = k.sort((a, b) => a.price - b.price);
+  return sorted[Math.round(q * (sorted.length - 1))];
+};
+
+//Return the product matching by uuid in list of products
+const findProductByUuid = (products, id) => {
+  return products.find(x => x.uuid == id);
+};
+
+/*
+ Apply filters on products
+*/
+const applyfilter = (products, filters) => {
+  
+  var afterproducts = Object.assign([], products);
+  
+  //Brand filter
+  if ((filters.brand!='all')&&(filters.brand!=undefined)){
+    var afterproducts = products.filter(k => k.brand===filters.brand);
+  };
+
+  //Recent filter
+  if ((filters.recent==true)){
+    var a = [];
+    afterproducts.forEach(function(product) {
+      let productDate = new Date(product.released);
+      let currentTime = new Date();
+      var twoWeeksAgo = currentTime.minusDays(14);
+      if (productDate > twoWeeksAgo) { a.push(product); }
+    })
+    afterproducts = a;
+  };
+
+  //Reasonable prices
+  if(filters.reasonable==true){
+    var afterproducts = products.filter(k => k.price<50);
+  }
+
+  //Sort filter
+  if ((filters.sort!='null')&&(filters.sort!=undefined)){
+    if (filters.sort=="price-asc"){ afterproducts.sort((a, b) => (a.price > b.price) ? 1 : -1)} //lowest highest
+    else if (filters.sort=="price-desc") {afterproducts.sort((a, b) => (a.price > b.price) ? -1 : 1)}
+    else if (filters.sort=="date-asc") {afterproducts.sort((a, b) => (new Date(a.released) > new Date(b.released)) ? -1 : 1)}
+    else if (filters.sort=="date-desc") {afterproducts.sort((a, b) => (new Date(a.released) > new Date(b.released)) ? 1 : -1)}
+  };
+
+  console.log(afterproducts);
+
+  return afterproducts;
+};
+
+
+
 /**
  * Render list of products
  * @param  {Array} products
  */
-const renderProducts = products => {
+const renderProducts = (products, filters) => {
+  //Applaying filters
+  var afterproducts = applyfilter(products, filters);
+
+  //Create products section
   const fragment = document.createDocumentFragment();
   const div = document.createElement('div');
-  const template = products
+  div.className = "product-item";
+  var template = afterproducts
     .map(product => {
       return `
-      <div class="product" id=${product.uuid}>
-        <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
-        <span>${product.price}</span>
+      <div class="product" id=${product.uuid} alt=${product.brand}>
+      <img class="product-img" src="${product.photo}">
+        <div class="product-desc">
+          <span class="product-brand">${product.brand}</span>
+          <a href="${product.link}"  class="product-name">${product.name}</a>
+          <span  class="product-price">${product.price}</span>
+          <label class="heart-label"><input type="checkbox" class="fav-check" value=${product.uuid}>❤</label>
+        </div>
       </div>
     `;
     })
     .join('');
 
+  //Si aucun produit
+  if (afterproducts.length==0){
+    template = `<div class="noproduct">
+                  <p>Il n'y a pas de produits correspondant à votre rechrche sur cette page :(<br>Rendez-vous à la page suivante !</p>
+                </div>`;
+  }
+  
   div.innerHTML = template;
   fragment.appendChild(div);
-  sectionProducts.innerHTML = '<h2>Products</h2>';
+  sectionProducts.innerHTML = '';
   sectionProducts.appendChild(fragment);
+
+  //Set all favorite products to red
+  var a = document.getElementById("products").querySelectorAll('.fav-check').forEach(item => {
+    if (favoriteProducts.filter(p => p.uuid == item.value).length > 0) { //if in favoriteProducts => heart is red, checkbox is checked
+      item.parentElement.style.color="rgb(201, 96, 47)";
+      item.checked = true;
+    };
+  });
+  var a = document.getElementById("products").querySelectorAll('.product-img').forEach(item => {
+    console.log(item);
+  });
+
+  //Add event listener on fav's checkboxes
+  fav = document.getElementById("products").querySelectorAll('.fav-check').forEach(item => {
+    item.addEventListener("click", event => {
+      var p = findProductByUuid(currentProducts,event.target.value);
+      if (event.target.checked) {
+        favoriteProducts.push(p);
+        item.parentElement.style.color="rgb(201, 96, 47)";
+      }
+      else {
+        favoriteProducts = favoriteProducts.filter(k => k.uuid != event.target.value);
+        item.parentElement.style.color="black";
+      }
+    });
+  });
+
+  console.log(afterproducts);
 };
 
 /**
- * Render page selector
+ * Render Page selector
  * @param  {Object} pagination
  */
 const renderPagination = pagination => {
@@ -87,19 +209,35 @@ const renderPagination = pagination => {
 };
 
 /**
- * Render page selector
+ * Render Indicators Page selector
+ * @param  {Object} products
  * @param  {Object} pagination
  */
-const renderIndicators = pagination => {
+const renderIndicatorsPagination = (pagination) => {
   const {count} = pagination;
-
   spanNbProducts.innerHTML = count;
 };
 
-const render = (products, pagination) => {
-  renderProducts(products);
+/**
+ * Render Indicators Products selector
+ * @param  {Object} products 
+ * @param  {Object} filters
+ */
+const renderIndicatorsProducts = (products, filters) => {
+  var productscopy = Object.assign([], products);
+  var p = applyfilter(products, filters);
+  spanNbDisplayedProducts.innerHTML = p.length;  
+  p50.innerHTML = percentile(products,0.5).price;
+  p90.innerHTML = percentile(products,0.9).price;
+  p95.innerHTML = percentile(products,0.95).price;
+  lastRelease.innerHTML = productscopy.sort((a, b) => (new Date(a.released) > new Date(b.released)) ? -1 : 1)[0].released;
+};
+
+const render = (products, pagination, filters) => {
   renderPagination(pagination);
-  renderIndicators(pagination);
+  renderIndicatorsPagination(products, pagination);
+  renderIndicatorsProducts(products, filters);
+  renderProducts(products, filters);
 };
 
 /**
@@ -113,11 +251,154 @@ const render = (products, pagination) => {
 selectShow.addEventListener('change', event => {
   fetchProducts(currentPagination.currentPage, parseInt(event.target.value))
     .then(setCurrentProducts)
-    .then(() => render(currentProducts, currentPagination));
+    .then(() => render(currentProducts, currentPagination, currentFilters));
 });
 
+//Select the page to display
+selectPage.addEventListener('change', event => {
+  fetchProducts(parseInt(event.target.value), selectShow.value)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination, currentFilters));
+});
+
+//Select the brand to display
+selectBrand.addEventListener('change', event => {
+  currentFilters.brand = event.target.value;
+  fetchProducts(currentPagination.currentPage, selectShow.value)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination, currentFilters));
+});
+
+//Select the recent products
+selectRecent.addEventListener('change', event => {
+  if(event.target.checked) {
+    currentFilters.recent=true;
+  } else {
+    currentFilters.recent=false;
+  };
+  fetchProducts(currentPagination.currentPage, selectShow.value)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination, currentFilters));
+});
+
+//Select the reasonable price
+selectReasonable.addEventListener('change', event => {
+  if(event.target.checked) {
+    currentFilters.reasonable=true;
+  } else {
+    currentFilters.reasonable=false;
+  };
+  fetchProducts(currentPagination.currentPage, selectShow.value)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination, currentFilters));
+});
+
+//Select the brand to display
+selectSort.addEventListener('change', event => {
+  currentFilters.sort = event.target.value;
+  fetchProducts(currentPagination.currentPage, selectShow.value)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination, currentFilters));
+});
+
+
+//Display page
 document.addEventListener('DOMContentLoaded', () =>
   fetchProducts()
     .then(setCurrentProducts)
-    .then(() => render(currentProducts, currentPagination))
+    .then(() => render(currentProducts, currentPagination, currentFilters))
 );
+
+
+
+function displayCheck(event) {
+  var p = findProductByUuid(currentProducts,event.target.value);
+  console.log(event.target.value);
+  if (event.target.checked) {
+    favoriteProducts.push(p);
+    console.log(event.target.checked);
+    console.log(favoriteProducts);
+  }
+  else {
+    favoriteProducts = favoriteProducts.filter(k => k.uuid != event.target.value);
+    console.log(event.target.checked);
+    console.log(favoriteProducts);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// products.forEach((el) => {console.log(el.price);})
+  // p50.innerHTML = percentile(products, .50).price;
+  // p90.innerHTML = percentile(products, .90).price;
+  // p95.innerHTML = percentile(products, .95).price;
+
+
+
+
+
+
+
+
+
+  /**
+   * Animations
+   */
+
+//NAVBAR
+
+  const navbar = document.querySelector(".navbar");
+  const menu = document.querySelector(".menu-list");
+  const cancelBtn = document.querySelector(".cancel-btn");
+  const menuBtn = document.querySelector(".menu-btn");
+
+  menuBtn.onclick = () => {
+      menu.classList.add("active")
+      menuBtn.classList.add("hide")
+  }
+
+  cancelBtn.onclick = () => {
+      menu.classList.remove("active")
+      menuBtn.classList.remove("hide")
+  }
+
+  window.onscroll = () => {
+      var logo = document.getElementById("navbar-logo");
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+      this.scrollY > 20 ? logo.style.display = "none" : logo.style.display = "block";
+      // this.scrollY > 20 ? navbar.style.backgroundColor = white : console.log("coucou");
+  }
+
+
+  //FILTER
+  // const filterContainer = document.querySelector(".gallery-filter"),
+  //   galleryItems = document.querySelectorAll(".gallery-item");
+
+  //   filterContainer.addEventListener("click", (event) =>{
+  //   if(event.target.classList.contains("filter-item")){
+  //       // deactivate existing active 'filter-item'
+  //       filterContainer.querySelector(".active").classList.remove("active");
+  //       // activate new 'filter-item'
+  //       event.target.classList.add("active");
+  //       const filterValue = event.target.getAttribute("data-filter");
+  //       galleryItems.forEach((item) =>{
+  //       if(item.classList.contains(filterValue) || filterValue === 'all'){
+  //           item.classList.remove("hide");
+  //           item.classList.add("show");
+  //       }
+  //       else{
+  //           item.classList.remove("show");
+  //           item.classList.add("hide");
+  //       }
+  //       });
+  //   }
+  //   });
